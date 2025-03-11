896681ca784093c8ec8d108fbc2bcabc0530c024
its-manny <manuel@kochgruber.co.uk>
2025-03-11 19:21:40 +0000

    ch1p21, added comment with STRATEGY pattern definition

--- src/Duck/MiniDuckSimulator.java
+++ src/Duck/MiniDuckSimulator.java
@@ -1,5 +1,10 @@
 package Duck;
 
+/**
+ * Implements the STRATEGY design pattern
+ * The strategy pattern defines a family of algorithms, encapsulates each one, and makes them interchangeable.
+ * Strategy lets the algorithm vary independently of clients that use it.
+ */
 public class MiniDuckSimulator {
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
